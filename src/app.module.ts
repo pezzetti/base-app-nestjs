cc165eac9628bda58cd974df44c345eec8417e2a
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2019-12-02 21:53:23 +0100

    ormconfig now constructs the entities path dyn

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ORMConfig } from "./ormconfig";
 
 @Module({
-    imports: [UsersModule, TypeOrmModule.forRoot()],
+    imports: [UsersModule, TypeOrmModule.forRoot(ORMConfig)],
     controllers: [AppController],
     providers: [AppService],
 })
