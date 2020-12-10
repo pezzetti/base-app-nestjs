34111df0ea7b0265c99e6933250fda558c7b8a26
Rafael Pelliccioli <rafael.pezzetti@levvel.io>
2020-12-10 14:16:55 -0300

    chore: add swagger package and notation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
+    const options = new DocumentBuilder()
+        .setTitle("Base App API's")
+        .setDescription("List of API's available")
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
